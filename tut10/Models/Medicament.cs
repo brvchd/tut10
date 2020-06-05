c40c7860a0d3b1904c02bb0b65b3fcd1ddb46f91
Dmytro Borovych <s18963@pjwstk.edu.pl>
2020-06-05 13:13:56 +0200

    хай пиймав на комит

---
+++ tut10/Models/Medicament.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tut10.Models
+{
+    public class Medicament
+    {
+        [Key]
+        public int IdMedicament { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ tut10/Models/Prescription.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tut10.Models
+{
+    public class Prescription
+    {
+        [Key]
+        public int IdPrescription { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DueDate { get; set; }
+        public int IdPatient { get; set; }
+        public virtual Patient Patient { get; set; }
+        public int Id { get; set; }
+        public virtual Doctor Doctor { get; set; }
+    }
+}
